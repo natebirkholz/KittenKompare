df6a52348caf2f301422d45cd318614ef9ce3534
natebirkholz <nbirkholz@gmail.com>
2017-05-20 21:57:54 -0700

    Fixed bug with tableview not appearing.

--- Apply MJD/CatListingTableViewController.swift
+++ Apply MJD/CatListingTableViewController.swift
@@ -32,6 +32,9 @@ class CatListingTableViewController: UITableViewController {
             if let cats = maybeCats {
                 for cat in cats { self.data.append(cat) }
                 self.tableVIew.reloadData()
+                // Was trying to set itself offscreen slightly, fixed by adding 1
+                let topPoint = CGPoint(x: 0, y: 0 - self.tableVIew.contentInset.top + 1)
+                self.tableVIew.setContentOffset(topPoint, animated: true)
             }
         }
     }
@@ -55,13 +58,16 @@ class CatListingTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: kittyCell, for: indexPath) as! KittyTableViewCell
+
         let currentTag = cell.tag + 1
         cell.tag = currentTag
         cell.catName.text = data[indexPath.row].name ?? "Mystery cat!"
         cell.caption.text = data[indexPath.row].caption ?? ""
         if let catImage = data[indexPath.row].image { cell.catImage.image = catImage }
 
+        cell.setNeedsLayout()
+        cell.layoutIfNeeded()
+
         return cell
     }
-
 }
